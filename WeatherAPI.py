0cb7cf6bcdd41c0cb466458ba77c6bf5f3b46c6b
josephrobertlopez <josephrobertlopez@gmail.com>
2017-05-11 16:06:17 -0700

    requests n shit

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import urllib2
+import requests
 
 parser = argparse.ArgumentParser()  # Start args.
 parser.add_argument("-tgt", "--target", help="Zip code of weather data to be retrieved.")
@@ -26,3 +27,24 @@ else:
     quit(1)
 # End handling args
 
+def request(apikey, targets, sDate, eDate):
+    dict = {"appKey":"b1658f18-9f42-4dad-9178-a1adbc2c5d86", "targets":"93551", "startDate":"2010-1-01", "endDate":"2010-1-02"}
+    if apikey != '':
+        dict["appKey"] = apikey
+    if targets != '':
+        dict["targets"] = targets
+    if sDate != '':
+        dict["startDate"] = sDate
+    if eDate != '':
+        dict["endDate"] = eDate
+    parameters = ""
+    for key in dict:
+        parameters =  parameters + key + "=" + dict[key] + '&'
+    url = 'http://et.water.ca.gov/api/data?' + parameters + 'dataItems=day-asce-eto,day-eto,day-precip&unitOfMeasure=M;prioritizeSCS=N'
+    response = requests.get(url)
+
+    if (response.ok):
+        jData = json.loads(response.content)
+        print (jData)
+    else:
+        response.raise_for_status()
